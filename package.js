61ac7f8e7d63fdfb2941a574c0dd3628f8cfa63b
Michal Wanat <michalwanat@gmail.com>
2016-01-26 18:19:49 +0100

    dump version

--- package.js
+++ package.js
@@ -3,7 +3,7 @@
 Package.describe({
     name: 'vazco:universe-selectize',
     summary: 'Universe select input standalone - with the appearance as selectize. It is for use without autoform.',
-    version: '0.0.16',
+    version: '0.0.17',
     git: 'https://github.com/vazco/meteor-universe-selectize.git'
 });
 
